ece2786a9dc1d8f6997dacffc7f4a8f430a1ec76
thiagoteberga <thiagoteberga@live.com>
2021-07-18 21:51:09 -0300

    Removendo Teste do Netlify do Arquivo de Configuracao

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import React from 'react';
 import Routes from './routes';
 
 function App(props) { //<> são fragments - TAGS VAZIAS
-  /*
-  fetch(process.env.REACT_APP_API_URL || 'http://localhost:8080/api/v1')
-  .then(() => console.log('DEU CERTO'))
-  .catch(() => console.log('DEU ERRO'))
-  */
     return (
       <Routes></Routes>
     );
